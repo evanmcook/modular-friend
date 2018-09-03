361855b18f3152266cc1899d8c1e691116fc07ce
reaganhenke <reaganhenke@gmail.com>
2018-09-02 21:11:14 -0400

    navbar

--- src/navbar/navbar.js
+++ src/navbar/navbar.js
@@ -6,10 +6,10 @@ class Navbar extends Component {
   render() {
     return (
       <div className="navbar">
-        <div className="home-div">
+        <div id="home-div">
           <NavLink activeClassName='hidden' exact to="/">Evan Cook</NavLink>
         </div>
-        <ul id="links">
+        <ul id="navbar-links">
           <li><NavLink activeClassName='active' to="/resume">resume</NavLink></li>
           <li><NavLink activeClassName='active' to="/portfolio">portfolio</NavLink></li>
           <li><NavLink activeClassName='active' to="/links">links</NavLink></li>
