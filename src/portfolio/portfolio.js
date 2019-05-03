100a0f20f37d46a2fba55f23a48010b3d4998988
Evan Cook <cook@Cookbook.local>
2019-05-02 21:43:19 -0400

    updates to the portfolio - figure out link css

--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -5,9 +5,65 @@ class Portfolio extends Component {
   render() {
     return (
       <div className="portfolio">
-        <p> this is my portfolio </p>
+       <p> 2019</p>
+        <div class = "row">
+          <div class = "column"> In early 2019, Evan did show control programming for <i>Pip's Island</i>.
+          <br></br> 
+          At the same time, he was the assistant sound designer, in charge of programming the QLab for audio playback.
+          </div>
+          <div class = "column"></div>
         
+        </div>
+        <p> 2018</p>
+        
+        <div class = "row">
+        <div class = "column"> In October and November of 2018, Evan assisted Ryan Hickey on the Signature Theatre's production of <i> Billy Elliot: The Musical</i> During his time on the production, Evan programmed a QLab workspace that allowed the musical director and mix engineer to simultaneously take cues on different cuelists without interrupting eachother. Read more about this Q-chindōgu here. </div>
+        <div class = "column"> </div>
+        </div>
+
+        <br></br>
+        <br></br>
+
+        <div class = "row">
+          <div class = "column"> <i>Undead Joe, Airplane Fucker</i> is a mixtape made entirely of cassette tapes found in thrift stores, combined with one book on tape found in a 'free' bin. <br></br>
+          It tells the story of a man who is raised from the dead, his relationship with his Kate, his mortality, his fear of commitment, and his airplanes.</div>
+          <div class = "column"><iframe width="100%" height="400" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/490991337%3Fsecret_token%3Ds-FFwby&color=%234c5050&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></div>
+        
+
+          
+        </div>
+
+        <br></br>
+        <br></br>
+
+
+        <div class = "row">
+        <div class = "column">In the summer of 2018, Evan designed <i>The Pavilion</i> at The Hub Theatre. 
+        <br></br>
+       <a href="https://dcmetrotheaterarts.com/2018/04/01/review-the-pavilion-at-the-hub-theatre/" target="_blank"> Reviewer David Siegel made special mention of the sound design in his review.</a>  
+        </div>
+        <div class = "column"></div>
+        </div>
+        
+
+        <p> 2017</p>
+        <div class = "row">
+        <div class = "column">Original music from a production of <i>The Love of the Nightingale</i> <br></br> <br></br> Director: Shirley Serotsky <br></br>Callan Theater<br></br>The Catholic University of America<br></br>Washington, DC</div>
+          <div class = "column">
+          <iframe width="100%" height="400" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/431384172&color=%234c5050&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe>
+          </div>
+        </div>
+        <br></br>
+        <br></br>
+        <div class = "row">
+        <div class = "column">  <p> <i>yelling box</i>  is the mostly-purposeful combination of two toys into a circuit that seems to be very upset. </p> </div>
+        
+        <div class = "column"> <iframe width="100%" height="400" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/319278009&amp;color=%23d0d3d5&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;show_teaser=true&amp;visual=true"></iframe></div>
+       
+        </div>
       </div>
+      
+
     );
   }
 }
