08b82cec452fb1c0314588e7aeeaddd4c2969ae3
Evan Cook <cook@Cookbook.local>
2020-06-16 19:12:01 -0400

    update june 2020 002 - portf update

--- src/portfolio/portfolio.js
+++ src/portfolio/portfolio.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import './portfolio.css';
+import asparagus from './evancook.audio_AsparagusBywhoKuh.png';
 import flambowPicture from './flambowOnCom.jpg';
 import billyPicture from './billyElliotCueStack.png';
 
@@ -11,9 +12,24 @@ class Portfolio extends Component {
    
        
        <h1 className="subtitle"> 2020</h1>
+
+       <div class = "prfItem">
+         <div> 
+         <h2>Asparagus Cookbook</h2>
+          Evan designed a pay-what-you-want pack of 60 arcade-inspired sound effects.
+          <p><a href="https://gum.co/asparaguscookbook" target="_blank" rel="noopener noreferrer"> It is available for download now.</a> </p>
+         </div>
+
+         <div className = "rObj"> 
+         <img src={asparagus} alt="cover art for the asparagus cookbook by @WhoKuh." width ="100%" ></img>
+         </div>
+
+        </div>
+       
        
        <div class = "prfItem">
-       <div> <h2>Modelland</h2>
+       <div> 
+        <h2>Modelland</h2>
           <p>In late 2019 and early 2020, Evan was the audio programmer and assistant sound designer for <i>Modelland</i>.</p>
     
           <p> He was in charge of programming each of the 6 QLab computers that handled audio playback, and the 1 QLab computer that handled audio control.</p>
@@ -32,7 +48,8 @@ class Portfolio extends Component {
 
       <div class = "prfItem">
         <div>
-        <h2>Sooner/Later</h2> <p>Original music from the DC premiere of Allyson Currin's <i>Sooner/Later</i>. <br></br> <br></br> Director: Gregg Henry <br></br>Sprenger Theater <br></br>Atlas Performing Arts Center<br></br>Washington, DC</p></div>
+        <h2>Sooner/Later</h2> 
+        <p>Original music from the DC premiere of Allyson Currin's <i>Sooner/Later</i>. <br></br> <br></br> Director: Gregg Henry <br></br>Sprenger Theater <br></br>Atlas Performing Arts Center<br></br>Washington, DC</p></div>
         
         <div className = "rObj" > <iframe title= "soonerMusic" width="100%" height="400" scrolling="no" frameborder="no" allow="autoplay" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/playlists/791894535%3Fsecret_token%3Ds-XM8TN&color=%234c5050&auto_play=false&hide_related=false&show_comments=true&show_user=true&show_reposts=false&show_teaser=true&visual=true"></iframe></div>
 
@@ -43,7 +60,18 @@ class Portfolio extends Component {
           <div> <h2>Pip's Island</h2><p>In early 2019, Evan did show control programming for <i>Pip's Island</i>.</p>
           
           <p>At the same time, he was the assistant sound designer, in charge of programming the four QLab workspaces that handled audio playback.</p>
-          </div>
+          
+          
+          
+            
+            <p><a href="https://www.livedesignonline.com/theatre/a-hero-s-journey-part-5-pip-s-island-show-control" target="_blank" rel="noopener noreferrer">
+            <i>Live Design</i>'s article about the production's show control mentions Evan's role as the show control programmer: </a> </p>
+            
+            <p class="quote">"The system had to be designed, and more importantly programmed, in such a way that allows up to four different audiences in the space at one time, without impacting each other,” he adds. “A lot of this logic lies in human discussion and observation, and Evan remains the authority for Pip’s Island.”
+            </p>
+            
+            </div>
+          
           <div className = "rObj"> <img src={flambowPicture} alt="A Flambow attends to the com line." width ="85%" ></img></div>
         
         </div>
